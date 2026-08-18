41fb320002239181d075000d7aa3e7711178c0fb
yip-lgtm <yip@ktsurveyors.com>
2026-08-18 22:14:28 +0800

    fix(art): force portrait to design/characters/image.png

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// Restore full engine from last good commit, then swap portrait to
+// Load last good engine, then force the penguin portrait to
 // design/characters/image.png (GitHub Pages only deploys src/).
 const ENGINE_SHA = 'ad2e2eac84a1cc714dd4e1e864e432f37de6ff83';
 const ENGINE_URL = `https://raw.githubusercontent.com/yip-lgtm/ki7409-game-jam-202608/${ENGINE_SHA}/src/main.js`;
@@ -9,9 +9,20 @@ const src = await fetch(ENGINE_URL).then((r) => {
   return r.text();
 });
 
-const patched = src
+const portraitFn = `
+function getPenguinSVG(expr) {
+  return '<img src="' + ${JSON.stringify(PORTRAIT_URL)} + '" alt="\u9ad8\u677e\u71c8(\u4f01\u9d5d\u7248)" class="penguin-img" draggable="false">';
+}
+function getPenguinPortrait(expr) {
+  return getPenguinSVG(expr);
+}
+`;
+
+const patched = portraitFn + src
   .replace("from './data/script.js'", `from '${new URL('./data/script.js', import.meta.url).href}'`)
   .replace("from './i18n.js'", `from '${new URL('./i18n.js', import.meta.url).href}'`)
+  .replace('function getPenguinSVG', 'function getPenguinSVG_UNUSED')
+  .replace('function getPenguinPortrait', 'function getPenguinPortrait_UNUSED')
   .replaceAll('assets/portraits/tomorin.png', PORTRAIT_URL);
 
 const blob = new Blob([patched], { type: 'text/javascript' });
